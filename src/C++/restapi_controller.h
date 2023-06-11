019c974894eacc80f27278eb2025fbee4e3ee903
Lukas Merkle <lukas.merkle@live.de>
2023-06-11 12:08:31 +0200

    adds endpoint for giessen

--- src/C++/restapi_controller.h
+++ src/C++/restapi_controller.h
@@ -40,11 +40,18 @@ public:
         auto dto = MyDto::createShared();
         dto->statusCode = 200;
         dto->message = "Hello World!";
-        gisela->funktionGiessen(30);
         return createDtoResponse(Status::CODE_200, dto);
     }
 
     // TODO Insert Your endpoints here !!!
+
+    ENDPOINT("POST", "/giessen", root) {
+        auto dto = MyDto::createShared();
+        dto->statusCode = 200;
+        dto->message = "Gießen gestartet!";
+        gisela->funktionGiessen(30);
+        return createDtoResponse(Status::CODE_200, dto);
+    }
 
 };
 
